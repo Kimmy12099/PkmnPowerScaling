edb4d5f57c27b5c911b38b76b1eebed0860cc3dd
Kimmy12099 <bidoofkingplaysgames@gmail.com>
2024-10-28 15:54:22 -0400

    EvoStagePlanning

--- PyFiles/Evolution.py
+++ PyFiles/Evolution.py
@@ -1,10 +1,14 @@
 # Question to answer: is there a correlation between evolutionary stages and base stat total of a generation 
 
-#1. Access PokemonCompletestats Excel file 
+#1. Create a new excel file called "evolutoary stage and pokemon_id"
+        #Columns: pokemon_id, pokemon_name, Stage 1/2/3, Evolves into:, base stat total of pokemon, generation
+        #for base stat total of Pokemon, use the PokemonCompleteStats.xlsx file; If Pokemon_id = pokemon_id, then put pokemon BST in a column 
 #2. Create a new column called "evolution stage (1, 2, 3)" and fill in the values
-#3. *IMPORTANT* find a program that shows the evolutionary stages of all Pokemon 
+#3. *IMPORTANT* find a website/data that shows the evolutionary stages of all Pokemon (https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_evolutionary_line)
+      #Use pip install requests beautifulsoup4 pandas openpyxl
+      #After you download everything, clean the data
 #4. Divide Pokemon into 9 generations 
 #5. Divide Pokemon into 3 evolutionary stages (combined with #4)
 #6. Find the average base stat total of each generation for each evolutionary stage 
 #7. Also count how many stage 1,2,3 Pokemon are in each generation too and/or averages 
-#8.  Data visualization for #6 and #7 (smth like matplotlib or seaborn)
+#8. Data visualization for #6 and #7 (smth like matplotlib or seaborn)
